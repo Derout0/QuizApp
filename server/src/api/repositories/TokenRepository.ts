fa85f3420a276cea8dc3190fbe5461c2356a76d0
Derout0 <dmitriy09050@gmail.com>
2024-09-01 15:28:04 +0300

    Refactoring the mapping of TypeScript model to SQL

--- server/src/api/repositories/TokenRepository.ts
+++ server/src/api/repositories/TokenRepository.ts
@@ -1,5 +1,4 @@
-import database from '@/config/DatabaseConfig.js'
-import { GeneratedTokenModel, TokenModel } from '@/api/models/TokenModel.js'
+import { TokenModel } from '@/api/models/TokenModel.js'
 import { BaseRepository, FieldMapping } from '@/api/repositories/BaseRepository.ts'
 
 const tokenFieldMapping: FieldMapping<TokenModel> = {
@@ -12,14 +11,5 @@ const tokenFieldMapping: FieldMapping<TokenModel> = {
 export class TokenRepository extends BaseRepository<TokenModel> {
     constructor() {
         super('tokens', tokenFieldMapping)
-    }
-
-    public async createToken(userId: number, tokens: GeneratedTokenModel): Promise<void> {
-        const { refreshToken, refreshTokenExpires } = tokens
-
-        await database.query('INSERT INTO '
-        + 'tokens (user_id, refresh_token, refresh_token_expires_at) '
-        + 'VALUES ($1, $2, $3)',
-        [userId, refreshToken, refreshTokenExpires])
     }
 }
